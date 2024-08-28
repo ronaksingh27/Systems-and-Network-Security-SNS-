8d717dcdb07ae7e25cd339b89cdd7c94b40151e2
Ronak Singh <ronaksingh2708@gmail.com>
2024-08-28 22:11:32 +0530

    shell script added

---
+++ scripts/create_folders.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter the number of assignments: "  num_assignments
+absolute_path=$(pwd)
+
+
+echo "Absolute path: $pwd"
+
+for(( i=1; i<=num_assignments ; i++ ))
+do
+    folder_name="$absolute_path/Prg${i}"
+    if [ -d "$folder_name" ]; then
+        echo "Folder '$folder_name' already exists , Skipping"
+    else
+        mkdir "$folder_name"
+        touch "$folder_name/prg${i}_bt21cse028.c"
+        echo "Folder '$folder_name' created"
+    fi
+done
